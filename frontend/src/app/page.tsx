7fe8dddbd832b97cbd37255d3e5fe923ac1300e1
Alberto Garcia <agarcia05@berkeley.edu>
2025-06-14 23:32:11 -0700

    test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 
 import { useEffect, useState } from 'react'
@@ -48,21 +47,6 @@ export default function Home() {
           h
       </div>
     </>
-=======
-import type { Post } from './models/post';
-
-
-export default async function Page() {
-  const res = await fetch('http://localhost:8000/posts')
-  const posts: Post[] = await res.json()
-
-  return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
->>>>>>> ea148bd475763f7041110fcd3ccfa664f276d33a
   )
 }
 
